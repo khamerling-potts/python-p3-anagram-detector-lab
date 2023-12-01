bdb13c8f68a6872df83cc2c45d9ec3f9a1e055a5
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-12-01 08:49:55 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, anagram):
+        self.anagram = anagram
+
+    def match(self, list):
+        matches = []
+        target = [char for char in self.anagram]
+
+        for word in list:
+            potential_match = [char for char in word]
+            if sorted(potential_match) == sorted(target):
+                matches.append(word)
+
+        return matches
